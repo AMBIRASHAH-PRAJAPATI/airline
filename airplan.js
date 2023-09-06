579316840e37668ebe1ec99c35139da9c60ac9e5
AMBIRASHAH-PRAJAPATI <amrajprajapato@gmail.com>
2023-09-06 23:36:44 +0530

    some modifications

--- airplan.js
+++ airplan.js
@@ -13,20 +13,37 @@ let openclosenavfun = ()=>{
 let opennav= document.getElementById("opennav");
 opennav.addEventListener("click", ()=>{ openclosenavfun();});
 
+// scroll function
+const scrollcontainer = document.querySelectorAll(".scroll");
+scrollcontainer.forEach(element => {
+  const leftmove = element.querySelector(".leftmove");
+  const rightmove = element.querySelector(".rightmove");
+  const scrollcontainer = element.querySelector(".scroll-container");
+  const scrollpic = element.querySelector(".scroll-pic");
+  const movewidth = scrollpic.clientWidth + parseInt(window.getComputedStyle(scrollpic).paddingLeft);
+  rightmove.addEventListener("click", () => {
+    scrollcontainer.scrollLeft += movewidth;
+  });
+  leftmove.addEventListener("click", () => {
+    scrollcontainer.scrollLeft -= movewidth;
+  });
+
+});
+
+
 // open footer
 let openclosefootfn = () => {
     let listsome = document.getElementById("listsome");
-    let openfooter = document.querySelector(".ft img");
     if(listsome.classList.contains("showlistsome")){
         listsome.classList.remove("showlistsome");
-      openfooter.style.transform = "rotate(0deg)";
+      openfooter.style.transform = "translate(-50%, -50%) rotate(0deg)";
 
     } else {
       listsome.classList.add("showlistsome");
-      openfooter.style.transform = "rotate(180deg)";
+      openfooter.style.transform ="translate(-50%, -50%) rotate(180deg)";
     }
   };
   
-  let openfooter = document.querySelector(".ft img");
+  let openfooter = document.getElementById("morefotter");
   openfooter.addEventListener("click", () => { openclosefootfn(); });
   
